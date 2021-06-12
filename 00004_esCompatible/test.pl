e0eb6f72c480c3f9cf17cb03ac52c81393fea213
juan9794@gmail.com <juan9794@gmail.com>
2021-06-12 22:39:04 +0000

    Mumuki Export on 2021-06-12 22:39:04 +0000

--- 00004_esCompatible/test.pl
+++ 00004_esCompatible/test.pl
@@ -1,22 +1,22 @@
-test(librianas_con_ascendente_en_piscis_son_compatibles_con_capricornianas_con_ascendiente_en_aries, [nondet]) :-
+test(personas_librianas_con_ascendente_en_piscis_son_compatibles_con_capricornianas_con_ascendiente_en_aries, [nondet]) :-
     esCompatible(luis, jose).
   
-  test(acuarianas_con_ascendente_en_sagitario_son_compatibles_con_leoninas_con_ascendente_en_geminis, [nondet]) :-
+  test(personas_acuarianas_con_ascendente_en_sagitario_son_compatibles_con_leoninas_con_ascendente_en_geminis, [nondet]) :-
     esCompatible(maria, sara).
   
-  test(leoninas_con_ascendente_en_aries_no_son_compatibles_con_personas_de_libra_sin_ascendente_en_sagitario, [nondet]) :-
+  test(personas_leoninas_con_ascendente_en_aries_no_son_compatibles_con_personas_de_libra_sin_ascendente_en_sagitario, [nondet]) :-
     esCompatible(isabel, mario).
   
-  test(librianas_con_ascendente_en_piscis_no_son_compatibles_con_leoninas_con_ascendente_en_geminis) :-
+  test(personas_librianas_con_ascendente_en_piscis_no_son_compatibles_con_leoninas_con_ascendente_en_geminis) :-
     \+ esCompatible(luis, sara).
   
   test(una_persona_no_es_compatible_con_si_misma) :-
     \+ esCompatible(luis, luis).
   
-  test(acuarianas_con_ascendente_en_sagitario_no_son_compatibles_con_leoninas_sin_ascendente_en_geminis) :-
+  test(personas_acuarianas_con_ascendente_en_sagitario_no_son_compatibles_con_leoninas_sin_ascendente_en_geminis) :-
     \+ esCompatible(maria, mario).
   
-  test(leoninas_con_ascendente_en_aries_no_son_compatibles_con_librianas_sin_ascendente_en_sagitario) :-
+  test(personas_leoninas_con_ascendente_en_aries_no_son_compatibles_con_librianas_sin_ascendente_en_sagitario) :-
     \+ esCompatible(isabel, sara).
   
   test(esCompatible_es_inversible_para_su_primer_parametro, [nondet]) :-
